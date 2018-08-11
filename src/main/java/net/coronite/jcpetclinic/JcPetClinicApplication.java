41a1145485a5c37f8d706bddb854448651103917
jcoro <jcoronite@hotmail.com>
2018-08-10 20:26:28 -0700

    Initial Project Add

---
+++ src/main/java/net/coronite/jcpetclinic/JcPetClinicApplication.java
@@ -0,0 +1,12 @@
+package net.coronite.jcpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcPetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/net/coronite/jcpetclinic/JcPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package net.coronite.jcpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JcPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
