3ca85d1dd524ad3dc79910f33ee16cc4f6699dea
jcoro <jcoronite@hotmail.com>
2018-08-13 22:31:59 -0700

    Refactor to common CRUD Interface. Closes #11

--- pet-clinic-data/src/main/java/net/coronite/jcpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/net/coronite/jcpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package net.coronite.jcpetclinic.services;
 
 import net.coronite.jcpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById (Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/net/coronite/jcpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/net/coronite/jcpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package net.coronite.jcpetclinic.services;
 
 import net.coronite.jcpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
