5b849a699d5341e6b44aabaa3394417105b16e8d
jcoro <jcoronite@hotmail.com>
2018-12-15 07:53:45 -0700

    Added PetTypes to bootstrap. Closes #32

--- pet-clinic-web/src/main/java/net/coronite/jcpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/net/coronite/jcpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package net.coronite.jcpetclinic.bootstrap;
 
 import net.coronite.jcpetclinic.model.Owner;
+import net.coronite.jcpetclinic.model.PetType;
 import net.coronite.jcpetclinic.model.Vet;
 import net.coronite.jcpetclinic.services.OwnerService;
+import net.coronite.jcpetclinic.services.PetTypeService;
 import net.coronite.jcpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,15 +15,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
+
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
