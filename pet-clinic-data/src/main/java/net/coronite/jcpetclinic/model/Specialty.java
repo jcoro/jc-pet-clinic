5596ab723d5e2c04001ba67399658e3ab5de9c84
jcoro <jcoronite@hotmail.com>
2018-12-15 07:03:19 -0700

    Added Specialty. Added to Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/net/coronite/jcpetclinic/model/Specialty.java
@@ -0,0 +1,19 @@
+package net.coronite.jcpetclinic.model;
+
+/**
+ * Specialty.java - DESCRIPTION
+ * Author: John Coronite
+ * Date: 2018-12-15
+ **/
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
