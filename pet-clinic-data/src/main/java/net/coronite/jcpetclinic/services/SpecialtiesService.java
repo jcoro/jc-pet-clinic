7888634d5b4f63486d15d75d61ee676c46a7cc63
jcoro <jcoronite@hotmail.com>
2019-01-07 00:32:55 -0700

    Added Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/net/coronite/jcpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,11 @@
+package net.coronite.jcpetclinic.services;
+
+import net.coronite.jcpetclinic.model.Specialty;
+
+/**
+ * SpecialtiesService.java - DESCRIPTION
+ * Author: John Coronite
+ * Date: 2019-01-07
+ **/
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/net/coronite/jcpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,41 @@
+package net.coronite.jcpetclinic.services.map;
+
+import net.coronite.jcpetclinic.model.Specialty;
+import net.coronite.jcpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * SpecialtyMapService.java - DESCRIPTION
+ * Author: John Coronite
+ * Date: 2019-01-07
+ **/
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
